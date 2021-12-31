f12cce3eb052646511e7a2f7981f4a2c8c32b9b2
Denise Lu <denise.lu@nytimes.com>
2021-12-30 23:40:14 -0500

    clean

--- first-last-music/scripts/build.py
+++
@@ -1,9 +0,0 @@
-from datadoc import datadoc as doc
-import json
-
-# Returns a python dictionary
-test = doc.fetchData('14f5N7bbyDQR-1koi6D61s00qgZTdkTxXgUKTzjBWbQo')
-
-# Open new datafile and dump in the json
-with open('../public/data/data.json','wb') as dataFile:
-	dataFile.write(json.dumps(test))
