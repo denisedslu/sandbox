9f89599a72f0261ee9aab54a78675284f0161014
Denise Lu <deniselu@Denises-MacBook-Air.local>
2023-10-13 22:37:35 -0400

    up

--- pavement-1933-2023/.svelte-kit/output/server/entries/pages/_layout.svelte.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const styles = "";
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="app"><main>${slots.default ? slots.default({}) : ``}</main> </div>`;
-});
-export {
-  Layout as default
-};
