e20a844489e8005d736fa93baa5ec3b57423550c
Denise Lu <deniselu@Denises-MacBook-Air.local>
2023-10-13 23:15:35 -0400

    paths

---
+++ pavement-1933-2023/_app/immutable/chunks/paths.430a0a1e.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1rl4vec)==null?void 0:s.base)??"/sandbox/pavement-1933-2023";var e;const t=((e=globalThis.__sveltekit_1rl4vec)==null?void 0:e.assets)??a;export{t as a,a as b};
